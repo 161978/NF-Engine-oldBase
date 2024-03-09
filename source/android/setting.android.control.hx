63a4cf80453b4710edef2f0e339aa2c1895ff271
161978 <122792113+161978@users.noreply.github.com>
2024-03-09 08:38:17 +0800

    Add files via upload
    Signed-off-by: 161978 <122792113+161978@users.noreply.github.com>

---
+++ source/android/setting.android.control.hx
@@ -0,0 +1,10 @@
+//put it at scripts files it will work
+//use for android control set
+
+function onCreate(){
+    ClientPrefs.data.spaceExtend = true; //android control for extra space
+	ClientPrefs.data.shiftExtend = true; //android control for extra Shift
+	ClientPrefs.data.hitboxLocation = 'Bottom'; //Top and middle
+	ClientPrefs.data.hitboxalpha = 0.2; //Hitbox alpha
+	ClientPrefs.data.VirtualPadAlpha = 0.75;  //VirtualPad alpha
+}
